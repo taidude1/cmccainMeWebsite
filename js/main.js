1eb3564a481e05c081af2b9c7adc7a97cd139b90
Connor <DeloreanCTM@Gmail.com>
2025-12-26 00:09:38 -0500

    REACT Chaos and other html

--- js/main.js
+++ js/main.js
@@ -2,4 +2,7 @@
 // You can later add theme toggles, filters, etc.
 
 console.log("Site loaded");
-    
+
+
+// let and const are the variables that are used in javascript, let can be anything
+// while const ensures that the var is constant
